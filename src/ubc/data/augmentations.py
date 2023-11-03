4691386175bcc2185c30b3bafedacd2ca38a2f8b
kiansierra <kiansierra90@gmail.com>
2023-11-03 11:45:16 +0100

    Add image augmentation options using omegaconf

--- src/ubc/data/augmentations.py
+++ src/ubc/data/augmentations.py
@@ -1,5 +1,6 @@
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+from omegaconf import DictConfig
 
 NORMALIZE_MEAN = [0.485, 0.456, 0.406]
 NORMALIZE_STD = [0.229, 0.224, 0.225]
@@ -7,21 +8,23 @@ NORMALIZE_STD = [0.229, 0.224, 0.225]
 __all__ = ["get_train_transforms", "get_valid_transforms"]
 
 
-def get_train_transforms(config):
-    return A.Compose(
-        [
-            A.Resize(config["img_size"], config["img_size"]),
+def get_train_transforms(config:DictConfig) -> A.Compose:
+    transforms = [
+            A.Resize(config.img_size, config.img_size),
+            A.HorizontalFlip(p=0.5) if config.get('flip', False)  else None,
+            A.VerticalFlip(p=0.5) if config.get('flip', False)  else None,
+            A.Rotate(limit=180, p=0.5) if config.get('rotate', False) else None,
             A.Normalize(mean=NORMALIZE_MEAN, std=NORMALIZE_STD, max_pixel_value=255.0, p=1.0),
             ToTensorV2(),
-        ],
-        p=1.0,
-    )
+        ]
+    transforms = [transform for transform in transforms if transform is not None]
+    return A.Compose(transforms, p=1.0)
 
 
-def get_valid_transforms(config):
+def get_valid_transforms(config:DictConfig)-> A.Compose:
     return A.Compose(
         [
-            A.Resize(config["img_size"], config["img_size"]),
+            A.Resize(config.img_size, config.img_size),
             A.Normalize(mean=NORMALIZE_MEAN, std=NORMALIZE_STD, max_pixel_value=255.0, p=1.0),
             ToTensorV2(),
         ],
