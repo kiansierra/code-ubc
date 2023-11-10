cc8d0678a8b36d45558bb0b84989bd067ab8b4d1
kiansierra <kiansierra90@gmail.com>
2023-11-10 11:14:30 +0100

    Refactor image splitting script to support multiple backends and parallel processing

--- scripts/split_images.py
+++ scripts/split_images.py
@@ -1,69 +1,58 @@
 
 import argparse
 import gc
+import multiprocessing as mp
 import os
 from pathlib import Path
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 import albumentations as A
+import hydra
 import matplotlib.pyplot as plt
 import numpy as np
+import pyvips
 from loguru import logger
+from ubc import PyVipsProcessor, TorchProcessor
 
 ROOT_DIR = Path("../input/UBC-OCEAN/")
 PROCESSED_DIR = Path("../input/UBC-OCEAN-PROCESSED/")
 
-
-def split_image(img:np.ndarray, split_size:int=512) -> Dict[Tuple[int, int], np.ndarray]:
-    output = {}
-    for i in range(0, img.shape[0], split_size):
-        for j in range(0, img.shape[1], split_size):
-            output[(i,j)] = img[i:i+split_size, j:j+split_size]
-    return output
-
-
-def save_image_splits(img:np.ndarray, path:Path, split_size:int=512) -> None:
-    splits = split_image(img, split_size)
-    clean_emptys = lambda x: {k:v for k,v in x.items() if v.std()>0}
-    splits = clean_emptys(splits)
-    logger.info(f'Splitting image {path.stem} in to {len(splits)} splits of size {split_size}')
-    for (i,j), split in splits.items():
-        plt.imsave(str(path / f'{i}_{j}.png'), split)
-
-
-def load_split_save(img_path:Path, save_folder:Path, split_size:int=512, resize_factor:float=0.25) -> None:
-    save_path = save_folder/ img_path.stem
-    save_path.mkdir(exist_ok=True, parents=True)
-    if len(list(save_path.glob('*.png'))) > 4:
-        logger.info(f'Image {img_path.stem} already split. Skipping.')
-        return None
-    for img_path in save_path.glob("*.png"):
-        os.remove(img_path)
-    img = plt.imread(str(img_path))
-    if resize_factor != 1 and max(img.shape) >4_000: #Exculde TMAs which are already small
-        max_size = int(max(img.shape)*resize_factor) 
-        logger.info(f'Resizing image {img_path.stem} of shape {img.shape} by factor {resize_factor} to max size {max_size}')
-        img = A.LongestMaxSize(max_size=max_size)(image=img)['image']
-        gc.collect()
-    
-    save_image_splits(img, save_path, split_size)
-    del img
-    gc.collect()
     
 def arguments():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--backend', type=str, default='pyvips')
     parser.add_argument('--save_folder', type=str, default=str(PROCESSED_DIR / 'train'))
-    parser.add_argument('--image_path', type=str, required=True)
+    parser.add_argument('--image_path', type=str, default=None)
+    parser.add_argument('--image_folder', type=str, default=None)
     parser.add_argument('--resize', type=float, default=0.25)
     parser.add_argument('--split_size', type=int, default=512)
+    parser.add_argument('--max_tiles', type=int, default=None)
+    parser.add_argument('--center_crop', action='store_true', default = False)
+    parser.add_argument('--num_processes', type=int, default = 6)
+    
     args = parser.parse_args()
     return args
 
 def main() -> None:
     args = arguments()
-    image_path = Path(args.image_path)
     save_folder = Path(args.save_folder)
-    load_split_save(image_path, save_folder, args.split_size, args.resize)
+    assert args.image_path or args.image_folder, "Must provide either image_path or image_folder"
+    assert args.backend in ['pyvips', 'torch'], "Backend must be one of ['pyvips', 'torch']"
+    if args.backend == 'torch':
+        processor = TorchProcessor(save_folder, args.resize, args.split_size,
+                                max_tiles=args.max_tiles, center_crop=args.center_crop)
+    else:
+        
+        processor = PyVipsProcessor(save_folder, args.resize, args.split_size,
+                                max_tiles=args.max_tiles, center_crop=args.center_crop)
+    if args.image_path:
+        processor.run(args.image_path)
+        return 
+    all_images = list(Path(args.image_folder).glob('*.png'))
+    with mp.Pool(processes=args.num_processes) as pool:
+        pool.map(processor.run, all_images)
+    
+    
     
 if __name__ == '__main__':
     main()
