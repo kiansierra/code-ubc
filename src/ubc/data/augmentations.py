15e5f47df1f86ff4c215555c959068d67dd60baf
kiansierra <kiansierra90@gmail.com>
2023-11-12 13:55:12 +0100

    Add image augmentations for blurs and dropouts

--- src/ubc/data/augmentations.py
+++ src/ubc/data/augmentations.py
@@ -7,10 +7,20 @@ NORMALIZE_STD = [0.229, 0.224, 0.225]
 
 __all__ = ["get_train_transforms", "get_valid_transforms"]
 
+def get_blurs():
+    blurs = [A.Blur(), A.GaussianBlur(), A.MotionBlur(), A.MedianBlur()]
+    return A.OneOf(blurs, p=0.5)
+
+def get_dropouts():
+    dropouts = [A.Cutout(num_holes=10, max_h_size=16, max_w_size=16), A.PixelDropout(dropout_prob=0.05), A.MotionBlur(), A.MedianBlur()]
+    return A.OneOf(dropouts, p=0.5)
+
 
 def get_train_transforms(config: DictConfig) -> A.Compose:
     transforms = [
         A.Resize(config.img_size, config.img_size),
+        get_blurs() if config.get("blur", False) else None,
+        get_dropouts() if config.get("dropout", False) else None,
         A.HorizontalFlip(p=0.5) if config.get("flip", False) else None,
         A.VerticalFlip(p=0.5) if config.get("flip", False) else None,
         A.Rotate(limit=180, p=0.5) if config.get("rotate", False) else None,
