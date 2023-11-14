40b4c2a2f4319f130e600f2f0a652eaa69ffdcdf
kiansierra <kiansierra90@gmail.com>
2023-11-14 11:21:52 +0100

    Refactor split_images_crop.py script

--- scripts/split_images_crop.py
+++ scripts/split_images_crop.py
@@ -12,7 +12,6 @@ import pandas as pd
 from dotenv import load_dotenv
 from loguru import logger
 from omegaconf import DictConfig
-
 from ubc import ComponentsProcessor
 
 load_dotenv()
@@ -26,16 +25,18 @@ def apply_processor(processor:ComponentsProcessor, record):
 @hydra.main(config_path="../src/ubc/configs/preprocess", config_name="crop-512-0.5", version_base=None)
 def main(config:DictConfig) -> None:
     save_folder = Path(config.save_folder)
-    assert config.get('image_folder'), "Must provide either image_path or image_folder"
+    assert config.get('image_folder'), "Must provide either image_folder"
+    logger.info(f"Loading {config.dataframe_path}")
     df = pd.read_parquet(config.dataframe_path)
     df_group = df.groupby('image_id', as_index=False)['components'].agg(list).rename(columns={'components': 'crop_position'})
-    df_group['path'] = df_group['image_id'].apply(lambda x: str(ROOT_DIR/ 'train_images' / f"{x}.png"))
+    logger.info(f"Using Images in  {config.image_folder}")
+    df_group['path'] = df_group['image_id'].apply(lambda x: f"{config.image_folder}/{x}.png")
     records = df_group[['path', 'crop_position']].to_dict("records")
     processor = ComponentsProcessor(save_folder, config.resize_factor, config.split_size,
                             max_tiles=config.max_tiles, center_crop=False)
     start_time = time.time()
     partial_apply_processor = partial(apply_processor, processor)
-    logger.info(f"Processing {config.image_folder} total files {len(df_group['path'].unique())} started at {time.ctime()}")
+    logger.info(f"total files {len(df_group['path'].unique())} started at {time.ctime()}")
     if config.num_processes > 1:
         with mp.Pool(processes=config.num_processes) as pool:
             pool.map(partial_apply_processor, records)
